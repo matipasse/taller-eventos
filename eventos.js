bf830b305f654505ba249bb436f5c4091fcfe382
matipasse <matiaspasserini51@gmail.com>
2023-08-13 13:58:02 -0300

    Evitar propagacion de alertas

--- eventos.js
+++ eventos.js
@@ -1,9 +1,13 @@
 const div = document.querySelector("div");
+const boton = document.querySelector('button')
 
-div.addEventListener('click', (e) => {
+div.addEventListener('click', () => {
     alert('Hola! Soy el div');
-    e.stopPropagation();
   })
 
+boton.addEventListener('click', (e) => {
+  alert('Hola!');
+  e.stopPropagation();
+})
 
 
